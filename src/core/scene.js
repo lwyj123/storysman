243ef33ce7d87e7e2410678dfad9076c70c1344b
lwio <443474713@qq.com>
2017-10-18 22:58:15 +0800

    finished load scene

--- src/core/scene.js
+++ src/core/scene.js
@@ -1,8 +1,47 @@
 import config from 'config'
 import gameJson from 'game/game.json'
 import axios from 'axios'
+import yfm from 'yfm' // A simple to use YAML Front-Matter parsing and extraction Library
 
-const getSceneContent = function (scene) {
+// load scene file by its filename
+
+const runScene = function(hash) {
+  var scene = parse(hash);
+  let promise = Promise.resolve(scene);
+  // loadScene(scene) => parsed, renderScene(parsed) => undefined
+  return promise.then(loadScene)
+    .then(renderScene)
+
+  function parse (hash) {
+    var path = hash.slice(1);
+    var segments = path.split('/');
+    gistId = segments.shift();
+    var scene = segments.join('/');
+
+    if (scene === '') {
+      return 'index';
+    }
+
+    return scene;
+  };
+}
+
+const loadScene = function(scene) {
+  let promise = _getSceneContent(scene)
+    .then(_extractYFM.bind(this, scene))
+    .then(_initScene);
+  return promise
+}
+// render scene by its parsed object(extracted by yfm)
+const renderScene = function(parsed) {
+  
+}
+
+
+/*
+****Load part
+*/
+const _getSceneContent = function (scene) {
   let fileName = `${scene}.markdown`
   var file = gameJson[files][fileName];
 
@@ -12,22 +51,46 @@ const getSceneContent = function (scene) {
   return axios.get(fileURL)
 };
 
-
-const extractYFM = function(scene, content) {
+// extract scene style from scene file's content
+const _extractYFM = function(scene, content) {
+  // see the https://www.npmjs.com/package/yfm
   var parsed = yfm(content);
   if (parsed.context.style !== undefined) {
     injectSceneStyle(scene, parsed.context.style);
   }
+  function injectSceneStyle(scene, content) {
+    let head = document.getElementsByTagName('head')[0];
+    let style = document.createElement('style');
+    style.id = scene + '-style';
+    style.type = 'text/css';
+    style.innerHTML = content;
+    head.appendChild(style);
+  };
   return parsed;
 }
 
-const initScene = function(sceneContent) {
-
+// run init function of the scene file
+const _initScene = function(parsed) {
+  if (parsed.context.init !== undefined) {
+    parsed.context.init();
+  }
+  return parsed;
 }
 
+/*
+****Render part
+*/
+const _playTrack = function (parsed) {
+  if (parsed.context.track !== undefined) {
+    if (currentTrack !== undefined && !currentTrack.paused) {
+      $(currentTrack).animate({ volume: 0 }, 1000, playSceneTrack.bind(this, parsed.context.track));
+    } else {
+      playSceneTrack(parsed.context.track);
+    }
+  }
+  return parsed;
+};
 
 export {
-  getSceneContent,
-  extractYFM,
   initScene
 }
