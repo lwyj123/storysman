164a0545295250a6c7521971555514a20dcedf0e
lwio <443474713@qq.com>
2017-10-20 19:17:14 +0800

    fix the test.html and Storysman

--- src/core/core.js
+++ src/core/core.js
@@ -17,22 +17,9 @@ var init = function () {
   window.onhashchange = function() {
     runScene(document.location.hash);
   }
-  /*var scene = parse(document.location.hash);
-
-  return q($.getJSON('http://localhost:8080/game/game.json'))
-    .then(function (gist) {
-      files = gist.files;
-      return initUI(scene);
-    }, function (xhr) {
-      toggleLoading(false);
-      toggleError(true, xhr.statusText);
-    });*/
 };
-Storysman.game = gameJson;
 
-
-
-Storysman.test = function() {
+Storysman.init = function() {
   init();
   runScene('');
 }
